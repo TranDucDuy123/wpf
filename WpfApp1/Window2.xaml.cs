631bb8c0aab73ae497cf42d97facc9eb2f449551
duy1233 <dyly399@gmail.com>
2024-11-25 17:31:27 +0700

    first commit

---
+++ WpfApp1/Window2.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    public partial class Window2 : Window, INotifyPropertyChanged
+    {
+        private bool _isRepeatEnabled;
+
+        public bool IsRepeatEnabled
+        {
+            get => _isRepeatEnabled;
+            set
+            {
+                _isRepeatEnabled = value;
+                OnPropertyChanged(nameof(IsRepeatEnabled));
+                OnPropertyChanged(nameof(IsRepeatPanelVisible));
+            }
+        }
+
+        public bool IsRepeatPanelVisible => IsRepeatEnabled;
+
+        public Window2()
+        {
+            InitializeComponent();
+            DataContext = this;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
